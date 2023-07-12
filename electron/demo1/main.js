314e38db166453154476665586f4fa1b1c4e3a9c
yxln <1032554797@qq.com>
2023-07-12 23:34:54 +0800

    尝试用electron做一个简单音乐播放器，播放云端音乐

---
+++ electron/demo1/main.js
@@ -0,0 +1,27 @@
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        webPreferences: {
+            contextIsolation: false,                                // 设置为false，才能在渲染进程中使用electron api
+            preload: path.join(__dirname, 'preload.js')             // 预加载js
+        }
+    });
+
+    win.loadFile('index.html');
+    win.webContents.openDevTools();
+}
+
+app.whenReady().then(() => {
+    createWindow();
+
+})
+
+app.on('window-all-closed', () => {
+    if(process.platform !== 'darwin') {
+        app.quit();
+    }
+})
